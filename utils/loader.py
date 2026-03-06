a018e015836cf60008c1653c62817384b09e9ebd
Mridul Lata <mridullata04@gmail.com>
2026-03-07 01:07:38 +0530

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,13 +1,8 @@
 import joblib
-import os
 
 def load_text():
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-    tokens_path = os.path.join(BASE_DIR, "models", "tokens.joblib")
-
-    tokens = joblib.load(tokens_path)
+    tokens = joblib.load("models/tokens.joblib")
 
     text = " ".join(tokens)
 
